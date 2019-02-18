768018935419c354d6e3d2b88621859a053caca5
James Tanton <james.tanton@elliemae.com>
2019-02-17 19:06:06 -0800

    update

--- get-solarinfo.py
+++ get-solarinfo.py
@@ -2,7 +2,9 @@
 ## usage: python get-solarinfo.py [-h] -e endpoint
 ##
 ## Python script to get enphaseenergy system summary using Enlighten API
-##		James Tanton 9/26/2018
+##		James Tanton
+#		Created : 9/26/2018
+#		Updated : 2/17/2019
 ## 		Help on getting started - https://developer.enphase.com/docs/quickstart.html	
 ##		
 ##		Your KEY and user values will be unique to your system of course
@@ -19,14 +21,20 @@
 import requests, json, pprint, os, argparse
 
 def getcontent(URL):
+	"""
+
+	:rtype: data
+	"""
+	print (URL)
 	#print URL # Debug line comment out or remove
 	r = requests.get(URL) #Fetching response from URL
-	#print 'get processed' # Debug line as well
-	#print r.content # Display unformated json response
+	print ('get processed') # Debug line as well
+	print (r.content) # Display unformated json response
 	data = json.loads(r.text) # Format as JSON
-	print 'raw data: \n' + r.content 
-	print '\n pprinted json:'
+	print ('raw data: \n' + str(r.content) )
+	print ('\n pprinted json:')
 	pprint.pprint(data)
+	#rtype: data
 	return data
 
 # Sample URL 
@@ -44,7 +52,7 @@ requiredArgs.add_argument('-e', action='store',
                           help='Name of the Enlighten Systems endpoint. e.g. consumption_lifetime,consumption_stats,energy_lifetime, envoys, index, inventory, monthly_production, production_meter_readings, rgm_stats, stats, summary, arrays?range=today&view=energy_production ',)
 inputargs = parser.parse_args()
 
-print inputargs.endpoint
+print (inputargs.endpoint)
 
 SITE = 'https://api.enphaseenergy.com/api/v2'
 #KEY = '?key=XXXXXX&user_id=XXXX'
@@ -56,4 +64,5 @@ KEY = "?key="+apikey+"&user_id="+userid
 # Call getcontent for systems. 
 APICALL = '/systems/' + siteid + inputargs.endpoint
 URL=SITE+APICALL+KEY
+print ( URL )
 getcontent(URL)
